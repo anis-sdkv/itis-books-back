e1c92a29dd104b914af5206713c2d606294c5678
Anis <sadikowas@yandex.ru>
2025-06-10 14:15:17 +0300

    updates

--- books_back/settings.py
+++ books_back/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -47,6 +47,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=10),   # 5 минут — по умолчанию
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),     # 1 день — по умолчанию
+    # Другие настройки...
 }
 
 MIDDLEWARE = [
